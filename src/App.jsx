1c2d898c6a73e31f8f0e19db8492f23ff87f8344
Thiago <thiago.gui@hotmai.com>
2025-05-07 00:00:47 -0400

    projeto final

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux'; // 👉 Importa useSelector
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
 
 function App() {
-  
   const [showProductList, setShowProductList] = useState(false);
 
   const handleGetStartedClick = () => {
@@ -16,34 +15,51 @@ function App() {
     setShowProductList(false);
   };
 
+  // 👉 Acessa os itens do carrinho do Redux
+  const cart = useSelector(state => state.cart.items);
+  const totalQuantity = cart.reduce((sum, item) => sum + item.quantity, 0);
+
   return (
     <div className="app-container">
+      {/* 👉 Header fixo com o carrinho */}
+      <header className="header">
+        <div className="logo">Paradise Nursery</div>
+        <div className="cart-icon-container">
+          <img src="cart-icon.png" alt="Cart" className="cart-icon" />
+          {totalQuantity > 0 && (
+            <span className="cart-count">{totalQuantity}</span>
+          )}
+        </div>
+      </header>
+
       <div className={`landing-page ${showProductList ? 'fade-out' : ''}`}>
         <div className="background-image"></div>
         <div className="content">
-         <div className="landing_content">
-         <h1>Welcome To Paradise Nursery</h1>
-          <div className="divider"></div>
-          <p>Where Green Meets Serenity</p>
-         
-          <button className="get-started-button" onClick={handleGetStartedClick}>
-            Get Started
-          </button>
-         </div>
-          <div className="aboutus_container">
-          <AboutUs/>
-          </div>
-          </div>
+          <div className="landing_content">
+            <h1>Welcome To Paradise Nursery</h1>
+            <div className="divider"></div>
+            <p>Where Green Meets Serenity</p>
 
+            <button className="get-started-button" onClick={handleGetStartedClick}>
+              Get Started
+            </button>
+          </div>
+          <div className="aboutus_container">
+            <AboutUs />
+          </div>
+        </div>
       </div>
+
       <div className={`product-list-container ${showProductList ? 'visible' : ''}`}>
-        <ProductList onHomeClick={handleHomeClick}/>
+        <ProductList onHomeClick={handleHomeClick} />
       </div>
     </div>
   );
 }
 
 export default App;
+
+
 
 
 
